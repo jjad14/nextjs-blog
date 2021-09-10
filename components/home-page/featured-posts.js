0c894b435663365c56ce794ce3323c3e0dccaf08
Dennis <dmarlo96@live.ca>
2021-09-10 00:06:45 -0400

    Featured Posts using dummy data

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PostsGrid from '../posts/posts-grid';
 
 import classes from './featured-posts.module.css';
 
-const FeaturedPosts = () => {
-	return <div></div>;
+const FeaturedPosts = ({ posts }) => {
+	return (
+		<section className={classes.latest}>
+			<h2>Featured Posts</h2>
+
+			<PostsGrid posts={posts} />
+		</section>
+	);
 };
 
 export default FeaturedPosts;
