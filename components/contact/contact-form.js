24d9296ac08837776bc1fddd418376dfd0aa69b0
Dennis <dmarlo96@live.ca>
2021-09-11 01:00:15 -0400

    Sending contact form data to api route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './contact-form.module.css';
 
 const ContactForm = () => {
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const [message, setMessage] = useState('');
+
+	const sendMessageHandler = (e) => {
+		e.preventDefault();
+
+		fetch('/api/contact', {
+			method: 'POST',
+			body: JSON.stringify({ email, name, message }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	};
+
 	return (
 		<section className={classes.contact}>
-			<h1>Have a question?</h1>
-			<form className={classes.form}>
+			<h2>Do you Have a question?</h2>
+			<form className={classes.form} onSubmit={sendMessageHandler}>
 				<div className={classes.controls}>
 					<div className={classes.control}>
 						<label htmlFor='email'>Your Email</label>
-						<input type='email' id='email' required />
+						<input
+							type='email'
+							id='email'
+							required
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
 					</div>
 					<div className={classes.control}>
 						<label htmlFor='name'>Your Name</label>
-						<input type='text' id='name' required />
+						<input
+							type='text'
+							id='name'
+							required
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
 					</div>
 				</div>
 				<div className={classes.control}>
 					<label htmlFor='message'>Your message</label>
-					<textarea name='message' id='message' rows='5'></textarea>
+					<textarea
+						name='message'
+						id='message'
+						rows='5'
+						required
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}></textarea>
 				</div>
 				<div className={classes.actions}>
-					<button>Send Message</button>
+					<button type='submit'>Send Message</button>
 				</div>
 			</form>
 		</section>
---
+++ pages/api/contact.js
@@ -0,0 +1,32 @@
+const handler = (req, res) => {
+	if (req.method == 'POST') {
+		const { email, name, message } = req.body;
+
+		if (
+			!email ||
+			!email.includes('@') ||
+			!name ||
+			name.trim() === '' ||
+			!message ||
+			message.trim() === ''
+		) {
+			res.status(422).json({ message: 'Invalid form input' });
+			return;
+		}
+
+		const newMessage = {
+			email,
+			name,
+			message
+		};
+
+		console.log(newMessage);
+
+		res.status(201).json({
+			message: 'Successfully sent message',
+			message: newMessage
+		});
+	}
+};
+
+export default handler;
