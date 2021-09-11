c5ea0b93d8da6cbf67159b272273acc3ccd05f0b
Dennis <dmarlo96@live.ca>
2021-09-11 01:09:36 -0400

    Saving message to db

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
 	if (req.method == 'POST') {
 		const { email, name, message } = req.body;
 
@@ -20,7 +22,30 @@ const handler = (req, res) => {
 			message
 		};
 
-		console.log(newMessage);
+		let client;
+
+		try {
+			client = await MongoClient.connect(process.env.MY_MONGO_URI);
+		} catch (error) {
+			res.status(500).json({ message: 'Could not connect to database' });
+			return;
+		}
+
+		const db = client.db();
+
+		try {
+			const result = await db
+				.collection('messages')
+				.insertOne(newMessage);
+
+			newMessage.id = result.insertedId;
+		} catch (error) {
+			client.close();
+			res.status(500).json({ message: 'Could not connect to database' });
+			return;
+		}
+
+		client.close();
 
 		res.status(201).json({
 			message: 'Successfully sent message',
